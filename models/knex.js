215d0eaf7490a61884e144f5ade351e0baae960a
Ibecheozor Chimuanya Bruce <chimuanyaibechozor@gmail.com>
2023-04-01 23:13:04 +0100

    restructure codebase

--- models/knex.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config({path: '../.env'})
-const config = require('./config');
-
-const environment = config.app.environment; // 'development' or 'production'
-const conf = require('../config/knexfile')(environment)
-module.exports = require('knex')(conf)
--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const { Model } = require("objection");
 
 
 
