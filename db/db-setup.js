2de8ead69177bb06b06bc4c94723f42d7c445e02
Ibecheozor Chimuanya Bruce <chimuanyaibechozor@gmail.com>
2023-04-07 22:29:39 +0100

    updated db-setup.js

--- db/db-setup.js
+++ db/db-setup.js
@@ -1,10 +1,13 @@
 const knex = require('knex');
 const knexConfig  = require('../config/knexfile');
 const { Model } = require('objection');
+const config = require('../config/config');
+
+environment = config.app.environment
 
 function setupDB() {
     // use the development env
-    const db = knex(knexConfig.development)
+    const db = knex(knexConfig[environment])
     Model.knex(db);    
 }
 
